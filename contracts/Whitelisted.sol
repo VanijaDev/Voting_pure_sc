af2132aabd4ff89aeed20810650a119ef5490165
Ivan Solomichev <vanijatko.social@gmail.com>
2018-11-04 23:03:24 +0200

    Added Whitelisted

---
+++ contracts/Whitelisted.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.25;
+
+import "./Ownable.sol";
+
+/**
+ * @dev Used simplier way (without OpenZeppelin Roles) 
+ */
+contract Whitelisted is Ownable {
+    mapping(address => bool) public whitelist;
+    
+    modifier onlyWhitelisted() {
+        require(whitelist[msg.sender], "ERROR: not whitelisted");
+        _;
+    }
+    
+    function addToWhitelist(address _address) public onlyOwner {
+        require(_address != address(0), "ERROR: address cannot be 0");
+        require(!whitelist[_address], "ERROR: address is already whitelisted");
+        
+        whitelist[_address] = true;
+    }
+    
+    function removeFromWhitelist(address _address) public onlyOwner {
+        require(_address != address(0), "ERROR: address cannot be 0");
+        require(whitelist[_address], "ERROR: address is not whitelisted");
+        
+        delete whitelist[_address];
+    }
+}
